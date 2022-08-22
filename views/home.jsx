7d3ee3f141091f84794783d8c8da3c55d0684d56
Jeffery Kryzewski <jeffkryzewski@Jefferys-Air.lan>
2022-08-21 20:41:55 -0400

    added burger image

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/eiliv-sonas-aceron-pu6b4yIlQF4-unsplash.jpg" alt="Great Burger!" />
+                <div>
+                  Photo by <a href="https://unsplash.com/@shootdelicious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eiliv-Sonas Aceron</a> on <a href="https://unsplash.com/s/photos/burger?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+              </div>
               <a href="/places">
                   <button className="btn-primary">Places Page</button>
               </a>
